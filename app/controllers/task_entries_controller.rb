ea9935a00aac66746587f8af1b9f03a30212b885
Wnokes <westonroynokes@gmail.com>
2018-05-28 10:38:37 -0600

    fridays work

--- app/controllers/task_entries_controller.rb
+++ app/controllers/task_entries_controller.rb
@@ -1,2 +1,78 @@
 class TaskEntriesController < ApplicationController
+	before_action :set_task_entry, only: [:show, :edit, :update, :destroy, :toggle_status]
+	access all: [:show, :index], user: {except: [:destroy, :new, :create, :update, :edit, :toggle_status]}, admin: :all
+
+	def index
+		@task_entries = TaskEntry.page(params[:page])
+	end
+
+	def new
+		@task_entry = TaskEntry.new
+	end
+
+	def create
+		@task_entry = TaskEntry.new(task_entry_params)
+		if @task_entry.save
+
+
+      	# TODO REDIRECT TO NOT A SHOW PAGE FOR TASK ENTRY
+
+
+      	redirect_to task_entry_path(@task_entry)
+      else
+      	render :new
+      end
+  end
+
+  def edit
+  end
+  
+  def update
+  	if @task_entry.update(task_entry_params)
+  		@task_entry.save
+
+
+      	# TODO REDIRECT TO NOT A SHOW PAGE FOR TASK ENTRY
+
+      	redirect_to @task_entry, notice: 'Your task entry was edited successfully'
+      else
+      	render :edit
+      end
+  end
+
+  def show
+  	@task_entry = TaskEntry.friendly.find(params[:id])
+  end
+  
+  def destroy
+  	if @task_entry.destroy
+  		redirect_to task_entries_path, notice: 'Your task_entry was destroyed successfully'
+  	else
+  		render :show, notice: 'Your task_entry could not be destroyed'
+  	end
+  end
+
+
+  # DO WE NEED A TOGGLE STATUS? PROBABLY? TODO FIGURE OUT SPECIFICS ON TOGGLE STATUS LATER
+
+  def toggle_status
+  	if @task_entry.draft?
+  		@task_entry.published!
+  	elsif @task_entry.published?
+  		@task_entry.draft!
+  	end      
+  	redirect_to task_entry_path, notice:  "#{@task_entry.title} status has been updated."
+  end
+
+
+  private
+
+  def set_task_entry
+  	@task_entry = TaskEntry.find(params[:id])
+  end
+
+  def task_entry_params
+  	params.require(:task_entry).permit(:title, :start_time, :end_time )
+  end
+
 end
--- app/models/task_entry.rb
+++ app/models/task_entry.rb
@@ -1,3 +1,17 @@
 class TaskEntry < ApplicationRecord
   belongs_to :TimeEntries
+
+  validate_presece_of :start_time
+
+  def self.switchtask ctask
+  	ctask.end_time = time now
+
+  	ctask save
+
+  	ctask = TaskEntry.new
+
+  	ctask.start_time = time.now
+
+  	
+  end
 end
