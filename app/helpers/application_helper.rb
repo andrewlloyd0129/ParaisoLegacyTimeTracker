7944d8cab072723581753d02c6913c75f83ec0e6
andrewlloyd0129 <andrewlloyd0129@gmail.com>
2018-05-28 11:42:50 -0600

    wired up gritter

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+
+  def alerts
+    alert = (flash[:alert] || flash[:error] || flash[:notice])
+
+    if alert
+      alert_generator alert
+    end
+  end
+
+  def alert_generator msg
+    js add_gritter(msg, sticky: false, time: 1000)
+  end
+
 end
